2fb2bd2bf0b45667a797e070ef367df6e1cee31a
Paul McKellar <paul.mckellar@gmail.com>
2013-12-15 09:32:09 -0800

    local variables

--- ping.rb
+++ ping.rb
@@ -1,11 +1,10 @@
 require "open-uri"
 
-urls = [
-  "http://www.lulwat.com"
-]
-
-
 def ping
+  urls = [
+    "http://www.lulwat.com"
+  ]
+
   urls.each do |url|
     puts "#{DateTime.now} #{url} #{URI.parse(url).read[0..60]}"
   end
