c564c9b094899ee3514f7d2f4106edc4f9dcb46e
Paul McKellar <paul.mckellar@gmail.com>
2013-12-15 09:30:32 -0800

    run every 60 seconds

--- ping.rb
+++ ping.rb
@@ -4,6 +4,14 @@ urls = [
   "http://www.lulwat.com"
 ]
 
-urls.each do |url|
-  puts "#{DateTime.now} #{url} #{URI.parse(url).read[0..60]}"
+
+def ping
+  urls.each do |url|
+    puts "#{DateTime.now} #{url} #{URI.parse(url).read[0..60]}"
+  end
+end
+
+while true
+  ping()
+  sleep 60
 end
